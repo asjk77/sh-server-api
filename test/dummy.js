ad67537a6dbe8a7dc513b6b9620acd678752de4b
MacBook <san@MacBookui-MacBook.local>
2021-05-22 01:49:46 +0900

    add dummy

---
+++ test/dummy.js
@@ -0,0 +1,9 @@
+/*******************************************************************************
+* File Name      : dummy.js                                                    *
+* Created Date   : Saturday, May 22nd 2021, 1:46:13 am                         *
+* Author         : Hwang SanHo                                                 *
+* Email          : tksgh1000@naver.com                                         *
+* Copyright (c) 2021 San Form Co.                                              *
+*******************************************************************************/
+
+// Description : this is test dummy file
